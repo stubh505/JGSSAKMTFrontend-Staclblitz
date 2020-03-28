ca2c8db6e2b1dff5812af539bfa9784c4c203254
stubh505 <kaustubh.sarkar505@gmail.com>
2020-03-28 16:06:55 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# jgssakmtfrontend-fxkc7h
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/jgssakmtfrontend-fxkc7h)
---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'jgssakmt-frontend',
+  templateUrl: './app.component.html',
+  styleUrls: [ './app.component.css' ]
+})
+export class AppComponent  {
+  name = 'Angular';
+}
